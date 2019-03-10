a7815a3672bae59df9eccc81c44c3d2e54dd6379
Eeegry <kolesnikov6@inbox.lv>
2019-03-10 23:14:27 +0200

    Update index.php
    Functions

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Hellow World Blen
+<?php
+    $x = -15;
+    echo abs ($x)."<br/>";
+    $y = 49.5141323;
+    echo round ($y, 3)."<br/>";
+    echo ceil($y)."<br/>";
+    echo floor($y)."<br/>";
+
+    echo mt_rand(1, 20)."<br/>";
+    echo min(12, 23, -2, -5, 10, -4, 0)."<br/>";
+    echo max(12, 23, -2, -5, 10, -4, 0)."<br/>";
+
+    $z = 1;
+
+    echo round(sin($z),2);
+?>
