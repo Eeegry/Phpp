111cf488079e03370071354144f3be5fb5456629
Eeegry <kolesnikov6@inbox.lv>
2019-03-11 20:51:01 +0200

    Update check.php
    Added some features

--- check.php
+++ check.php
@@ -4,11 +4,15 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $name = $_POST['name'];
     $message = $_POST['message'];
+    echo("Email: ");
     print_r($email);
     echo "<br />";
+    echo("Name: ");
     print_r($name);
     echo "<br />";
+    echo("Message: ");
     print_r($message);
     echo "<br />";
+    print_r($_POST);
 }
 ?>
